82818d370f8e721340d5187d471454e1f161f412
vasyl-malion <malionv@ukr.net>
2020-09-14 17:30:10 +0300

    com

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  graph_url: 'https://graph.richland.im/graphql',
+  // graph_url: 'http://localhost:5000/graphql',
+  locize: {
+    projectId: '8b824736-64d2-44e3-8fa5-87c1543bc725',
+  }
+};
+
+export default config;
